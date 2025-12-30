3ea1ae60e30c81d08f3abeeba5348d42a97c6313
unknown <macarena.l@suitedb.com>
2025-12-30 17:15:23 -0300

    modify customer creation object

--- Extensions/Workspace/SDB_Klaviyo_Integration/Modules/SDBKlaviyoIntegration/JavaScript/SuiteDB.SDB_Klaviyo_Integration.EntryPoint.js
+++ Extensions/Workspace/SDB_Klaviyo_Integration/Modules/SDBKlaviyoIntegration/JavaScript/SuiteDB.SDB_Klaviyo_Integration.EntryPoint.js
@@ -17,7 +17,7 @@ define('SuiteDB.SDB_Klaviyo_Integration.EntryPoint', [
 		mountToApp: function mountToApp(container) {
 			var cartComponent = container.getComponent('Cart')
 			var env = container.getComponent('Environment');
-			var testingMode = true;
+			var testingMode = false;
 
 			if (testingMode) {
 				sendRequest = function (data) {
@@ -43,18 +43,62 @@ define('SuiteDB.SDB_Klaviyo_Integration.EntryPoint', [
 				return this.track('trackProductView', product)
 			}
 
+			function getCustomerData() {
+				var profile = ProfileModel.getInstance();
+debugger
+				var isLoggedIn = profile.get('isLoggedIn') == 'T';
+
+				var subCategoryHtml = profile.get('customfields').find(function (sub) {
+					return sub.name == 'custentity_customer_sub_category';
+				}).html;
+
+				var parser = new DOMParser();
+				var doc = parser.parseFromString(subCategoryHtml, "text/html");
+				var subCategory = doc.querySelector('input[name="inpt_custentity_customer_sub_category"]').value;
+
+				var address = {}
+				if (isLoggedIn) {
+					var addrObj = profile.get('addressbook').find(function (a) {
+						return a.defaultbilling == 'T';
+					});
+
+					address.address1 = addrObj.addr1;
+					address.address2 = addrObj.addr2 + addrObj.addr3;
+					address.country = addrObj.country;
+					address.zip = addrObj.zip;
+					address.city = addrObj.city;
+					address.region = addrObj.state;
+				}
+
+				return {
+					email: profile.get('email')+'2',
+					organization: isLoggedIn ? profile.get('companyname') : 'guest',
+					phone_number: profile.get('phone'),
+					location: address,
+					properties: {
+						customerID: isLoggedIn ? profile.get('name') : 'guest',
+						type: isLoggedIn ? profile.get('isperson') == 'T' ? 'Individual' : 'Company' : 'guest',
+						status: profile.get('stage'),
+						category: isLoggedIn ? profile.get('customfields').find(function (sub) {
+							return sub.name == 'custentity_sdb_customer_for_webstore';
+						}).value : '',
+						subCategory: subCategory
+					}
+				};
+			}
+				
 			function sendRequest(data) {
 				var model = new KlaviyoModel()
 
-				var profile = ProfileModel.getInstance()
 				var item = data.product.get("item") ? data.product.get("item").attributes : null;
 
-				data.userData = {
-					email: profile.get('email'),
-					name: profile.get('firstname'),
-					last_name: profile.get('lastname'),
-					defaultName: profile.get('name')
-				}
+				data.userData = getCustomerData();
+				// data.userData = {
+				// 	email: profile.get('email'),
+				// 	name: profile.get('firstname'),
+				// 	last_name: profile.get('lastname'),
+				// 	defaultName: profile.get('name')
+				// }
 
 				if (item){
 				var images = item.itemimages_detail;
